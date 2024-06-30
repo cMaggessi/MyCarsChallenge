47a37f49c94cc5b14cf39830a4df3ca3d19d69e6
BuildTools <unconfigured@null.spigotmc.org>
2024-06-30 18:58:46 -0300

    updt: filtering allowed methods

--- src/main/java/com/cars/mycarsbackend/configuration/WebConfig.java
+++ src/main/java/com/cars/mycarsbackend/configuration/WebConfig.java
@@ -2,22 +2,18 @@ package com.cars.mycarsbackend.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig
-{
+public class WebConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET","POST","DELETE","PUT")
-                        .allowedOrigins("http://localhost:5173");
+                        .allowedMethods("GET", "POST", "DELETE", "PUT");
             }
         };
     }
