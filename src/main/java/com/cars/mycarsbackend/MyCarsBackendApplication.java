1dfa6964bafe2b258c10c4b0e33cc8df3f83261d
BuildTools <unconfigured@null.spigotmc.org>
2024-06-30 18:12:13 -0300

    config: adding cors policy(localhost)

--- src/main/java/com/cars/mycarsbackend/MyCarsBackendApplication.java
+++ src/main/java/com/cars/mycarsbackend/MyCarsBackendApplication.java
@@ -2,9 +2,6 @@ package com.cars.mycarsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MyCarsBackendApplication {
@@ -13,14 +10,5 @@ public class MyCarsBackendApplication {
         SpringApplication.run(MyCarsBackendApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:5173");
-            }
-        };
-    }
 
 }
