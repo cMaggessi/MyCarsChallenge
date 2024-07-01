8f287f596174cb9b0f5fddbc01d13f3340263a1f
BuildTools <unconfigured@null.spigotmc.org>
2024-06-30 21:33:51 -0300

    feat: adding findByMarca()

--- src/main/java/com/cars/mycarsbackend/repository/ModeloRepository.java
+++ src/main/java/com/cars/mycarsbackend/repository/ModeloRepository.java
@@ -1,10 +1,14 @@
 package com.cars.mycarsbackend.repository;
 
+import com.cars.mycarsbackend.model.Marca;
 import com.cars.mycarsbackend.model.Modelo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ModeloRepository extends JpaRepository<Modelo, Long> {
+    List<Modelo> findByMarca(Marca marca);
 
 }
