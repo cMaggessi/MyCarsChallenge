12a7ff9790b52ce27da20509152da41b24081c05
BuildTools <unconfigured@null.spigotmc.org>
2024-06-30 21:33:58 -0300

    feat: adding getModelosByMarca()

--- src/main/java/com/cars/mycarsbackend/service/ModeloService.java
+++ src/main/java/com/cars/mycarsbackend/service/ModeloService.java
@@ -7,8 +7,8 @@ import java.util.List;
 public interface ModeloService {
     List<ModeloDTO> findAll();
     ModeloDTO getModelo(Long id);
-
     ModeloDTO createModelo(ModeloDTO dto);
     ModeloDTO updateModelo(Long id, ModeloDTO dto);
     ModeloDTO deleteModelo(Long id);
+    List<ModeloDTO> getModelosByMarca(Long marcaId);
 }
