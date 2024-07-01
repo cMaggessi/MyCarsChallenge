5d4dd9b01ef4b613e4c39338db6cb40077d719f9
BuildTools <unconfigured@null.spigotmc.org>
2024-06-30 21:33:32 -0300

    updt: bug fix

--- src/main/java/com/cars/mycarsbackend/controller/MarcaController.java
+++ src/main/java/com/cars/mycarsbackend/controller/MarcaController.java
@@ -6,7 +6,6 @@ import com.cars.mycarsbackend.model.Marca;
 import com.cars.mycarsbackend.service.MarcaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
